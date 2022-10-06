1a439db5bf1da67d321c81c7ebb5b8fe4731d127
Alves Bernardo <bernardo.correia.alves@student.nhlstenden.com>
2022-10-06 14:38:17 +0200

    Second commi

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="styles/styles.css">
     <title>OUI</title>
 </head>
 <body>
@@ -13,7 +14,7 @@
 
         <?php
 
-        include_once "includes/navBar.php";
+        include_once "includes/files/html/navbar.html";
 
 
 
@@ -39,6 +40,45 @@
         ?>
 
     </header>
+
+
+
+<section id="mainPicture>">
+
+
+
+
+
+
+    <i class="fa-sharp fa-solid fa-arrow-down"></i>
+</section>
+
+
+<section id="ourSpecialties">
+
+        <h2 class="h2Title">Our Specialties</h2>
+
+    <div class="boxes">
+        <h4>Le croissant</h4>
+        <img src="includes/pictures/illustrations/illustrations/croissant/illustrations_yellow_croissant.png" alt="croissant">
+        <p>Our hand made croissant</p>
+    </div>
+
+    <div class="boxes">
+        <h4>Le Thé Francais</h4>
+        <img src="includes/pictures/illustrations/illustrations/tea/illustrations_yellow_tea.png" alt="tea">
+        <p>Tea from France</p>
+    </div>
+
+    <div class="boxes">
+        <h4>Le café Francais </h4>
+        <img src="includes/pictures/illustrations/illustrations/coffee/illustrations_yellow_coffee.png" alt="coffe">
+        <p>Hand brewed coffee</p>
+    </div>
+
+
+</section>
+
 
 </body>
 </html>
