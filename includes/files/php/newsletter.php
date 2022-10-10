44c385c5f9ede657872b2e5b7a2feb63b39fcf5d
Alex Schuepbach <alex.schuepbach@student.nhlstenden.com>
2022-10-10 12:45:34 +0200

    Create newsletter structure
    raw HTML done, css still needs something

--- includes/files/php/newsletter.php
+++ includes/files/php/newsletter.php
@@ -7,7 +7,18 @@
     </head> 
 
     <body>
-        
+        <div class="container">
+            <h1>Get updates on the cafe</h1>
+            <p>Sign up for news updates and a chance to win a free item off of our menu!</p>
+
+            <form>
+                <p><input type="text" id="fname" placeholder="First name"></p>
+                <p><input type="text" id="lname" placeholder="Last name"></p>
+                <p><input type="email" id="email" placeholder="Email address"></p>
+
+                <p><input type="submit" value="Sign up"></p>
+            </form>
+        </div>
     </body>
 
 </html>
