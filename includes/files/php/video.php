5d00051333d71dec4de75aa8b649978c7d5c2c3b
Alves Bernardo <bernardo.correia.alves@student.nhlstenden.com>
2022-10-18 18:14:02 +0200

    FirstVersion Of Nav phone

---
+++ includes/files/php/video.php
@@ -0,0 +1,18 @@
+
+<section id="vide">
+    <div class="coffeevideo">
+
+        <h1>How we make our coffee</h1><br>
+
+        <div id="flexVid">
+        <iframe width="1000" height="560" src="https://www.youtube.com/embed/ftzwByCgNUY" title="YouTube video player"
+                frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br>
+        </div>
+
+    </div>
+</section>
+
+
+
+
+
