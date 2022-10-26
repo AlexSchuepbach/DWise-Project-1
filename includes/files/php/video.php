62184fa04dcd7f8031d99284efb3b76b46014352
Alves Bernardo <bernardo.correia.alves@student.nhlstenden.com>
2022-10-26 14:38:18 +0200

    SecondToLastVersion

--- includes/files/php/video.php
+++ includes/files/php/video.php
@@ -12,6 +12,26 @@
     </div>
 </section>
 
+<section id="videoPhone">
+    <div class="coffeevideo">
+
+    <h1>How we make our coffee</h1><br>
+
+
+    <iframe id="phoneVideo" width="250" height="160"
+            src="https://www.youtube.com/embed/ftzwByCgNUY"
+            title="YouTube video player"
+            frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br>
+
+        <iframe id="phoneVideo" width="250" height="160"
+                src="https://www.youtube.com/embed/ftzwByCgNUY"
+                title="YouTube video player"
+                frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br>
+    </div>
+</section>
+
+
+
 
 
 
