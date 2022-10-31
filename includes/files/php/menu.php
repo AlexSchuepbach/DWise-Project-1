b6f84055346549a4bc6e5fd11d94dab7ed9fa7d8
GabrielGuevaraLopez <114413163+GabrielGuevaraLopez@users.noreply.github.com>
2022-10-31 12:21:41 +0100

    webpage backup uploaded

--- includes/files/php/menu.php
+++ includes/files/php/menu.php
@@ -22,82 +22,77 @@
         <h1 id="menuTitle">Our Kitchen and Bar</h1>
         <div class="box">
 
-        <div id="menufoodtext">
             <h2>Food</h2>
-        </div>
             <div id="menufood">
-            <ul id="menuList">
-                <div class="food">
-                <li>Pain, Croissant or Petit pain</li>
-                <li>Pain Naturel</li>
-                <li>Butter</li>
-                <li>Herb butter</li>
-                <li>Jam or Nutella</li>
-                <li>Brie or camembert</li>
-                <li>Cheese with walnuts</li>
-                <li>Cream cheese</li>
-                <li>Soft goat cheese</li>
-                <li>Smole Salmon</li>
-                <li>Tuna salad</li>
-                <li>Egg salad</li>
-                </div>
+                <ul id="menuList">
+                    <div class="food">
+                        <li>Pain, Croissant or Petit pain</li>
+                        <li>Pain Naturel</li>
+                        <li>Butter</li>
+                        <li>Herb butter</li>
+                        <li>Jam or Nutella</li>
+                        <li>Brie or camembert</li>
+                        <li>Cheese with walnuts</li>
+                        <li>Cream cheese</li>
+                        <li>Soft goat cheese</li>
+                        <li>Smole Salmon</li>
+                        <li>Tuna salad</li>
+                        <li>Egg salad</li>
+                    </div>
 
-                <div class="euros">
-                    <li>€1,30</li>
-                    <li>€2,30</li>
-                    <li>€2,80</li>
-                    <li>€3,30</li>
-                    <li>€4,50</li>
-                    <li>€4,70</li>
-                    <li>€4,80</li>
-                    <li>€4,80</li>
-                    <li>€5,80</li>
-                    <li>€4,80</li>
-                    <li>€4,80</li>
-                </div>
-            </ul>
+                    <div class="euros">
+                        <li>€1,30</li>
+                        <li>€2,30</li>
+                        <li>€2,80</li>
+                        <li>€3,30</li>
+                        <li>€4,50</li>
+                        <li>€4,70</li>
+                        <li>€4,80</li>
+                        <li>€4,80</li>
+                        <li>€5,80</li>
+                        <li>€4,80</li>
+                        <li>€4,80</li>
+                    </div>
+                </ul>
             </div>
-
-        <div id="menudrinkstext">
-            <h2>Drinks</h2>
-        </div>
+            <h2 id="drinks">Drinks</h2>
             <div id="menudrinks">
-            <ul>
-                <div class="drinks">
-                <li>Coffee and Tea</li>
-                <li>Cappucinno</li>
-                <li>Latte</li>
-                <li>Mocha</li>
-                <li>Espresso</li>
-                <li>Americano</li>
-                <li>Flat White</li>
-                <li>Thé</li>
-                <li>Cold drinks</li>
-                <li>Homemade lemonade</li>
-                <li>Jus d'orange</li>
-                <li>Jus de pomme</li>
-                <li>Ice Tea</li>
-                <li>Eau Minerale</li>
-                <li>Eau Petillante</li>
-                </div>
+                <ul>
+                    <div class="drinks">
+                        <li>Coffee and Tea</li>
+                        <li>Cappucinno</li>
+                        <li>Latte</li>
+                        <li>Mocha</li>
+                        <li>Espresso</li>
+                        <li>Americano</li>
+                        <li>Flat White</li>
+                        <li>Thé</li>
+                        <li>Cold drinks</li>
+                        <li>Homemade lemonade</li>
+                        <li>Jus d'orange</li>
+                        <li>Jus de pomme</li>
+                        <li>Ice Tea</li>
+                        <li>Eau Minerale</li>
+                        <li>Eau Petillante</li>
+                    </div>
 
-                <div class="euros1">
-                    <li>€3,00</li>
-                    <li>€3,00</li>
-                    <li>€3,00</li>
-                    <li>€2,00</li>
-                    <li>€3,50</li>
-                    <li>€2,80</li>
-                    <li>€2,00</li>
-                    <li>€3,00</li>
-                    <li>€2,30</li>
-                    <li>€2,30</li>
-                    <li>€2,50</li>
-                    <li>€2,00</li>
-                    <li>€2,30</li>
-                    <li>€2,30</li>
-                </div>
-            </ul>
+                    <div class="euros1">
+                        <li>€3,00</li>
+                        <li>€3,00</li>
+                        <li>€3,00</li>
+                        <li>€2,00</li>
+                        <li>€3,50</li>
+                        <li>€2,80</li>
+                        <li>€2,00</li>
+                        <li>€3,00</li>
+                        <li>€2,30</li>
+                        <li>€2,30</li>
+                        <li>€2,50</li>
+                        <li>€2,00</li>
+                        <li>€2,30</li>
+                        <li>€2,30</li>
+                    </div>
+                </ul>
             </div>
             <button id="Button">Download PDF</a></button>
         </div>
